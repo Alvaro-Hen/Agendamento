4f70ec489bfccc3c3b448d12d2ca082f3190d33b
Alvaro Henrique <ahseesha@gmail.com>
2025-12-26 21:32:10 -0300

    Listar Pacientes
    Ordenação alfabética da lista de pacientes

--- src/routes/listarPacientes.js
+++ src/routes/listarPacientes.js
@@ -7,9 +7,7 @@ listarPacientes.get('/api/pacientes', async (req, res) => {
     try{
         const db = await dbPromisse
 
-        const pacientes = await db.all('SELECT * FROM Pacientes')
-
-        console.log(pacientes)
+        const pacientes = await db.all('SELECT * FROM Pacientes ORDER BY nome COLLATE NOCASE')
 
         return res.status(200).json(pacientes)
     } catch(erro){
