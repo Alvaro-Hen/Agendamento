01ce7c672fbba7813b69c6fcc7d5070beebdc7e1
Michael Douglas <michaelm4douglas@gmail.com>
2026-01-01 17:03:37 -0300

    corrigir limpeza dos campos do formulario
    ajuste do preventDefault e reset dos campos do formulário

--- public/script/cadastrarPaciente.js
+++ public/script/cadastrarPaciente.js
@@ -3,15 +3,21 @@ window.addEventListener("load", main)
 function main(){
     
     const botao = document.getElementById('btn-agendar')
+    const nome = document.getElementById('nome');
+    const cpf = document.getElementById('cpf');
+    const dataNasc = document.getElementById('data');
+    const tel1 = document.getElementById('telefone');
+    const tel2 = document.getElementById('telefone2');
     
-    botao.addEventListener('click', async () => {
-        event.preventDefault();
+    botao.addEventListener('click', async (event) => {
+       event.preventDefault();
+        
         const dados = {
-            nome: document.getElementById('nome').value,
-            cpf: document.getElementById('cpf').value,
-            dataNasc: document.getElementById('data').value,
-            tel1: document.getElementById('telefone').value,
-            tel2: document.getElementById('telefone2').value,
+            nome: nome.value,
+            cpf: cpf.value,
+            dataNasc: dataNasc.value,
+            tel1: tel1.value,
+            tel2: tel2.value
         };
 
         try {
@@ -27,6 +33,12 @@ function main(){
 
             if (resposta.ok) {
                 alert("Sucesso: " + resultado.message);
+
+                nome.value = ""
+                cpf.value = ""
+                dataNasc.value = ""
+                tel1.value = ""
+                tel2.value = ""
             } else {
                 alert("Erro: " + resultado.error);
             }
@@ -34,10 +46,6 @@ function main(){
             console.error("Erro na requisição:", erro);
             alert("Não foi possível conectar ao servidor.");
         }
-        nome.value = ""
-        cpf.value = ""
-        dataNasc.value = ""
-        tel1.value = ""
-        tel2.value = ""
+        
     });
 }
