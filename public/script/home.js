958055a5cde07e0b10e08ab4c439d5911195d6cb
Michael Douglas <michaelm4douglas@gmail.com>
2026-01-06 18:28:19 -0300

    Home
    Listagem de Medicos do dia na tela Home

--- public/script/home.js
+++ public/script/home.js
@@ -7,15 +7,15 @@ window.addEventListener("load", async () => {
     
         try{
             const resposta = await fetch('/api/Pacientes');
-            const listaDePacientes = await resposta.json();
+            const listaDePacientesRecentes = await resposta.json();
             
-            const tbody  = document.getElementById('listarPacientes')
-            tbody.innerHTML = "";
+            const lista  = document.getElementById('listarPacientes')
+            lista.innerHTML = "";
 
 
-            listaDePacientes.forEach((element, index) => {
-                const tr = document.createElement("li")
-                tr.innerHTML = `
+            listaDePacientesRecentes.forEach((element, index) => {
+                const li = document.createElement("li")
+                li.innerHTML = `
                     <li class="item">
                         <div class="item-icone"><i class="fas fa-user-injured"></i></div>
                         <div class="item-detalhes">
@@ -26,9 +26,41 @@ window.addEventListener("load", async () => {
                     </li>
         
                 `;
-                tbody.appendChild(tr);
+                lista.appendChild(li);
             });
         }catch(erro){
             console.error("Erro ao mostrar elemento na tela ", erro)
         } 
+    }
+
+    async function listarMedicosdoDia() {
+
+        try {
+            const resposta = await fetch('/api/Profissionais')
+            const listarMedicosdoDia = await resposta.json();
+
+            const listaM = document.getElementById('listarMedicos')
+            listaM.innerHTML = "";
+
+            listarMedicosdoDia.forEach((element,index) => {
+                const li = document.createElement("li")
+                li.innerHTML = `
+                <li class="item">
+                    <div class="item-icone">
+                        <img src="/img/image 9.png" alt="Icone de Médico">
+                    </div>
+                    <div class="item-detalhes">
+                        <strong>Dr: ${element.login}</strong>
+                        <span>CRM: ${element.senha}</span>
+                        <span>Especialidade: ${element.cargo}</span>
+                    </div>
+                </li>
+
+                `;
+                listaM.appendChild(li);
+            });
+        } catch (error) {
+            console.error("Erro ao mostrar elemento na tela".erro)
+        }
+        
     }
--- src/routes/home.js
+++ src/routes/home.js
@@ -7,15 +7,19 @@ home.get('/api/home', async  (req,res) => {
     try {
         const db = await dbPromisse
 
-        const listarPacientes = await db.all("SELECT nome, cpf, dataNasc FROM Pacientes ORDER BY dataCadastro DESC LIMIT 10;" )
+        const listarPacientesRecentes = await db.all("SELECT nome, cpf, dataNasc FROM Pacientes ORDER BY dataCadastro DESC LIMIT 10;" );
+        const listarMedicosdoDia = await db.all("SELECT login, senha, cargo FROM Profissionais" );
 
-        return res.status(200).json(listarPacientes)
+        return res.status(200).json({listarPacientesRecentes,listarMedicosdoDia});
         
 
     } catch (error) {
         console.log("Erro ao buscar dados dos pacientes: ", error)
-        return res.status(500).json({error: "Erro ao Buscar dados dos pacientes"})
+        return res.status(500).json({error: "Erro ao Buscar dados da home"})
         
     }
 })
+
+
+
 
